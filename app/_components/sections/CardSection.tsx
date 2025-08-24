1d780a7c86076f4c3fbacf4f7756950aa68a4aeb
Archit-D2K <i-archit.rivastava@d2kindia.com>
2025-08-25 01:19:41 +0530

    fix alignment of card

--- app/_components/sections/CardSection.tsx
+++ app/_components/sections/CardSection.tsx
@@ -67,49 +67,48 @@ const CardSection = () => {
       className="min-h-screen py-20 bg-[#f9f8f3] flex items-center justify-center"
     >
       <div className="flex justify-center items-center relative w-full gap-10 px-10 md:px-20">
-        <div
-          ref={(el) => setCardRef(el, 0)}
-          className="w-[32%] h-[60vh] bg-black rounded-3xl flex items-center justify-center flex-col text-white text-center p-6 absolute z-10"
-          style={{ transform: "rotate(-8deg)" }} 
-        >
-          <Image
-            src="https://cdn.prod.website-files.com/681dfdff4444ca819f7050a2/68239a34145625a862ba3d54_icon-1.svg"
-            alt="Empowering Creators icon"
-            width={60}
-            height={60}
-            className="mb-6"
-          />
-          <p className="text-xl">Empowering Creators.</p>
-        </div>
+       <div
+  ref={(el) => setCardRef(el, 0)}
+  className="w-[32%] h-[60vh] bg-black rounded-3xl flex flex-col justify-between items-start text-white text-left p-6 absolute z-10"
+  style={{ transform: "rotate(-8deg)" }} 
+>
+  <Image
+    src="https://cdn.prod.website-files.com/681dfdff4444ca819f7050a2/68239a34145625a862ba3d54_icon-1.svg"
+    alt="Empowering Creators icon"
+    width={60}
+    height={60}
+  />
+  <p className="text-3xl">Empowering <br/> Creators.</p>
+</div>
 
-        <div
-          ref={(el) => setCardRef(el, 1)}
-          className="w-[32%] h-[60vh] bg-black rounded-3xl flex items-center justify-center flex-col text-white text-center p-6 relative z-20"
-        >
-          <Image
-            src="https://cdn.prod.website-files.com/681dfdff4444ca819f7050a2/68239b7ac5ddc2008b2da9b7_icon-2.svg"
-            alt="Transforming Publishing icon"
-            width={60}
-            height={60}
-            className="mb-6"
-          />
-          <p className="text-xl">Transforming Publishing.</p>
-        </div>
+<div
+  ref={(el) => setCardRef(el, 1)}
+  className="w-[32%] h-[60vh] bg-black rounded-3xl flex flex-col justify-between items-start text-white text-left p-6 relative z-20"
+>
+  <Image
+    src="https://cdn.prod.website-files.com/681dfdff4444ca819f7050a2/68239b7ac5ddc2008b2da9b7_icon-2.svg"
+    alt="Transforming Publishing icon"
+    width={60}
+    height={60}
+  />
+  <p className="text-3xl">Transforming <br/>
+     Publishing.</p>
+</div>
 
-        <div
-          ref={(el) => setCardRef(el, 2)}
-          className="w-[32%] h-[60vh] bg-black rounded-3xl flex items-center justify-center flex-col text-white text-center p-6 absolute z-10"
-          style={{ transform: "rotate(8deg)" }} 
-        >
-          <Image
-            src="https://cdn.prod.website-files.com/681dfdff4444ca819f7050a2/68239b7ab5708009ef8f649e_icon-3.svg"
-            alt="Reclaiming Canadian Media icon"
-            width={60}
-            height={60}
-            className="mb-6"
-          />
-          <p className="text-xl">Reclaiming Canadian Media.</p>
-        </div>
+<div
+  ref={(el) => setCardRef(el, 2)}
+  className="w-[32%] h-[60vh] bg-black rounded-3xl flex flex-col justify-between items-start text-white text-left p-6 absolute z-10"
+  style={{ transform: "rotate(8deg)" }} 
+>
+  <Image
+    src="https://cdn.prod.website-files.com/681dfdff4444ca819f7050a2/68239b7ab5708009ef8f649e_icon-3.svg"
+    alt="Reclaiming Canadian Media icon"
+    width={60}
+    height={60}
+  />
+  <p className="text-3xl">Reclaiming <br/> Canadian Media.</p>
+</div>
+
       </div>
     </section>
   );
